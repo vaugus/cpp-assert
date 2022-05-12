9d10490288e36f8f689db5f46e19e9305d60527e
vaugusto92 <vaugusto92@outlook.com>
2022-05-11 21:02:37 -0300

    refactor: cleanup the util functions

--- include/util.hpp
+++ include/util.hpp
@@ -6,76 +6,33 @@
 #include <sstream>
 #include <iostream>
 #include <regex>
+#include <initializer_list>
 #include "types.hpp"
 
 namespace Util
 {
-    static string create_include_path(std::string base_header) {
-        std::stringstream include_path;
-        include_path << "../include/" << base_header << ".hpp";
-        return include_path.str();
-    }
-
-    static string create_include_statement(std::string base_header)
-    {
-        std::stringstream include;
-        include << "#include \"" << create_include_path(base_header) << "\n";
-        return include.str();
-    }
-
-    static string double_quote(const std::string header) 
-    {
-        std::stringstream result;
-        result << "\"" << header << "\"";
-        return result.str();
-    }
-
-    static string indent(string text, int tabs)
-    {
-        std::stringstream indented_text;
-
-        for (int i = 0; i < tabs; i++)
-        {
-            indented_text << "\t";
-        }
-
-        indented_text << text;
-        return indented_text.str();
-
-    }
-
-    static string concat(string text, string concatenation)
-    {
-        std::stringstream result;
-        result << concatenation;
-        return result.str();
-    }
-
-    static void write_includes(vector<string> const& base_headers, std::ostream & stream)
-    {
-        string include_statement = "";
-        for (const string base : base_headers)
-        {
-            include_statement = create_include_statement(base);
-            stream << include_statement;
-        }
-    }
-
     static void write_line(std::ostream & output_stream, string line)
     {
         output_stream << line << "\n";
     }
 
-    static void copy_lines(std::ostream & output_stream, std::ifstream & input_stream, int number_of_lines)
+    static string concat(std::initializer_list<string> strings)
     {
-        string line;
-        for (int i = 0; i < number_of_lines; i++)
-        {
-            std::getline(input_stream, line);
-            write_line(output_stream, line);
-        }
+        std::stringstream result;
+        for (string element: strings)
+            result << element;
+
+        return result.str();
     }
 
+    static string parse_header(string const &unit_test_header)
+    {
+        const string folder = "test/";
+        const int path_prefix = unit_test_header.find(folder);
+        const string base = unit_test_header.substr(path_prefix + folder.length());
+        const int extension_position = base.find(".");
+        return base.substr(0, extension_position); 
+    }
 
     static void write_remaining_lines(std::ostream & output_stream, std::ifstream & input_stream)
     {
@@ -83,21 +40,6 @@ namespace Util
         {
             write_line(output_stream, line);
         }
-    }
-
-    static string replace_substring(string pattern, string target, string replacement)
-    {
-        return std::regex_replace(pattern, std::regex(target), replacement);
-    }
-
-    static string replace_substrings(string pattern, Replacements replacements) {
-        string result = pattern;
-
-        for (auto const& [key, val] : replacements) {
-            result = replace_substring(result, key, val);
-        }
-
-        return result;
     }
 
     static string capitalize_first_letter(string word)
