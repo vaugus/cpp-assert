d8ec05ea59ad10b36101e734db29331e75b60f86
d3adb5 <me@d3adb5.net>
2022-05-22 14:23:40 -0300

    feat: display usage message when given no args
    Display a basic usage message when the command is given no arguments
    upon being invoked. Currently very basic, but sets the stage for
    extension later on.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,23 @@
 #include "../include/core/command_facade.hpp"
 
+void usage()
+{
+	std::cerr <<
+		"This program expects the name of a command as a single "
+		"argument.\n";
+}
+
 int main(int argc, char **argv)
 {
-	if (argc == 2)
-	{
-		CommandFacade *facade = new CommandFacade();
-		auto commandFunction = facade->parse(argv[1]);
-		commandFunction();
-		delete facade;
+	if (argc < 2) {
+		usage();
+		return 0;
 	}
+
+	CommandFacade *facade = new CommandFacade();
+	auto commandFunction = facade->parse(argv[1]);
+	commandFunction();
+	delete facade;
 
 	return 0;
 }
